a6e6af62d46bb4b62603df7f30cc1468be959f56
Pavlo Mysyanovuch <pasha80676222267@gmail.com>
2016-06-29 14:03:04 +0300

    Update

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,7 @@
 angular.module("App", [])
     .controller("ItemCtrl", function($scope)
     {
-        // Загрузка
+       //DOWNLOADING
         if (localStorage["items"])
         {
             $scope.items = JSON.parse(localStorage["items"]);
@@ -10,93 +10,87 @@ angular.module("App", [])
         {
             $scope.items = [];
         }
+
       if(localStorage["comments"])
       {
           $scope.comments = JSON.parse(localStorage["comments"]);
       }
-      else {
-        $scope.comments = [];
-      }
-
+        //Adding items
         $scope.AddItem = function()
         {
             var index = $scope.items.length;
-            console.log(index);
-            $scope.items.push({id: index , name: $scope.itemName});
-            console.log($scope.itemName);
+            $scope.items.push({                       //pushing item data in local storage "items"
+                                  id: index ,
+                                  name: $scope.itemName
+            });
             localStorage["items"] = JSON.stringify($scope.items);//saving
+
         }
       
       $scope.selected = 0;
 
       $scope.select= function(index) {
-          $scope.selected = index;
-          if (index == $scope.selected) {
-              console.log($scope.selected);
-              for ($scope.comments.id = 0; $scope.comments.id < $scope.comments.length; $scope.comments.id++) {
-                  if ($scope.comments.id == $scope.selected) {
-                      console.log($scope.comments.id+"!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-                      $scope.comments[index] = JSON.stringify(localStorage["comments"]);//downloading comments
+          $scope.selected = index; //selected item
+
+        $scope.idComment = $scope.comments.id;
+
+              for ($scope.idComment = 0; $scope.idComment < $scope.comments.length; $scope.idComment++) {
+                  if ($scope.idComment == $scope.selected) {
+                      $scope.comments.id = JSON.parse(localStorage["comments"]);//downloading comments
                   }
               }
           }
-      }
-            $('.commentText').keydown(function (e) {
-                var key = e.which;
-                if(key == 13)  // the enter key code
-                {
-                    $scope.AddComment();
-                }
+            $('textarea').keypress (function(event){  // if press enter in the textarea
+
+              var keycode = (event.keyCode ? event.keyCode : event.which);
+              if(keycode === 13){// the enter key code
+                $scope.AddComment();                    //adding comment
+                $scope.countComments = $scope.comments.length;   // variable for counting comments
+              }
             });
+        $scope.indexCom = 0;
 
-        $scope.AddComment = function () {
+        $scope.AddComment = function () { // function for adding comments
 
-            var indexCom = 0;
-            $scope.comments.push({id: $scope.selected , idCom:indexCom ,text: $scope.comentText });
-            indexCom++;
-            console.log($scope.comentText,$scope.selected, indexCom);
+            $scope.comments.push({                          //pushing comments data to local storage "comments"
+                                    id: $scope.selected ,
+                                    idCom:$scope.indexCom ,
+                                    text: $scope.comentText
+            });
             localStorage["comments"] = JSON.stringify($scope.comments);//saving
-
            }
 
-
-
-
-        $scope.onKeyPress = function ($event) {
-            $scope.AddComment();
-        };
-
-
-        $scope.DeleteItem = function(item)
+      
+        $scope.DeleteItem = function(item)  //function for deleting item
         {
             var index = $scope.items.indexOf(item);
             if (index != -1) $scope.items.splice(index , 1);
             localStorage["items"] = JSON.stringify($scope.items);//saving
         }
-    })
-
-    .directive("myitem", function()
+})
+    .directive("myitem", function()   // directive for outputting items
     {
         return {
             restrict: "E",
             scope: {
               item: "=item",
-                delete: "=delete"
+              delete: "=delete"
             },
 
-            template:
+            template:   //contain name , count comments and delete button
             " <div " +
-            "class=\"info\"  >{{item.name}}</div>   <button class=\"btn-danger\" ng-click=\"delete(item)\">Delete</button> "
+            "class=\"info\"  >{{item.name}}</div> <div class=\"countComments\">{{ comments.length }}</div>   <button class=\"btn-danger\" ng-click=\"delete(item)\">Delete</button> "
         }
     })
 
-.directive("mycomment",function () {
+.directive("mycomment",function () {  //directive for outputting comments
   return {
     restrict:"E",
     scope: {
       comment:"=comment"
     },
-    template:"<div " + "class=\"userBlock\"  >{{comment.text}}</div>"
+    template:" <div class=\"comment\"> <div "+"class=\"commentImage\"></div> <div " + "class=\"commentBlock\"  >{{comment.text}} </div> </div>"
+    
   }
   });
 
